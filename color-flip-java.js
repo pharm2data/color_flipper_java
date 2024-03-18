43ec4d27547a480bca06b3c7d5b9c905eef27725
pharm2data <mollystephens321@gmail.com>
2024-03-17 20:48:34 -0400

    Create color-flip-java.js

---
+++ color-flip-java.js
@@ -0,0 +1,15 @@
+const colors = ["red", "blue", "green", "pink", "pink"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function () {
+// get random number between 0-3 colors [3]
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors [randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.random();
+}
